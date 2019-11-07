7f4cb5dc3deac83c74c4e1034f742539a2fa8b04
Vitalii Sashyn <vitaliis@wix.com>
2019-11-07 11:35:25 +0200

    Cleanup comments

--- src/CCCheckoutWidgetProvider.tsx
+++ src/CCCheckoutWidgetProvider.tsx
@@ -3,13 +3,13 @@ import * as React from "react";
 function noop(p) {}
 /**
  * **Internal API**
- * Checkout team. Should not be provided by API.
+ * hould not be provided by API.
  */
 const CheckoutContext = React.createContext({
   merchantId: "",
   isFormValid: false,
   setIsFormValid: noop,
-  startPayment: noop,
+  startPayment: noop
 });
 
 export { CheckoutContext };
@@ -21,7 +21,7 @@ interface CheckoutProviderProps {
 
 /**
  * **External API**
- * Checkout team. Should be used on top of all module.
+ * Should be used on top of all module.
  */
 export function CheckoutProvider(props: CheckoutProviderProps) {
   const [merchantId] = React.useState(props.merchantId);
--- src/withCheckout.tsx
+++ src/withCheckout.tsx
@@ -2,21 +2,22 @@ import * as React from "react";
 import { CheckoutContext } from "./CCCheckoutWidgetProvider";
 
 /**
- * Checkout team. Component to provide needed props
+ * Component to provide needed props
  * This HOC should be used to pass .startPayment method to child component
- * Can be used multiple times. 
+ * Can be used multiple times.
  * Provides method startPayment and isFormValid property
  */
 export function withCheckout(WrappedComponent) {
   return function(props) {
     return (
       <CheckoutContext.Consumer>
-        {checkoutProps => 
-          <WrappedComponent 
+        {checkoutProps => (
+          <WrappedComponent
             isFormValid={checkoutProps.isFormValid}
             startPayment={checkoutProps.startPayment}
-            {...props} 
-          />}
+            {...props}
+          />
+        )}
       </CheckoutContext.Consumer>
     );
   };
